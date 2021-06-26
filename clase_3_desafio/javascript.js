90c9170534cdae1687a8c377ba1c90f1a6319a2e
Matias Aramayo <matiasaramayo@hotmail.com>
2021-06-26 20:46:03 -0300

    first commit

---
+++ clase_3_desafio/javascript.js
@@ -0,0 +1,48 @@
+// EJERCICIOS DESAFIO CODER
+//EJERCICIO 1 
+//let pedirNumero = parseInt(prompt("Ingrese un Número"));
+
+//for (let i = 1; i <= 10; i++) {
+//    let resultado = pedirNumero * i;
+//    console.log(pedirNumero + " " + "X"+ " " +  i + " " + "=" + " "+ resultado);
+//}
+
+//EJERCICIO 2 
+//let entrada = prompt("Ingresar un dato");
+
+//while (entrada != "ESC") {
+//    alert("El usuario ingresó" + " " + entrada);
+//    entrada = prompt("Ingresar otro dato");
+//}
+
+
+//EJERCICIO 3
+//let ingresarNumero = parseInt(prompt("Ingrese un número"));
+
+//for (let i = 1; i <= ingresarNumero; i++) {
+//  console.log( i + " " + "Hola");
+//}
+
+
+//EJERCICIOS HACKERNOON
+//https://hackernoon.com/javascript-practical-coding-challenges-for-beginners-4bq3ugr
+
+//Coding challenge #1: Print numbers from 1 to 10
+
+//for (let i = 1 ; i <= 10 ; i++){
+//    console.log(i);
+//}
+
+//Coding challenge #2: Print the odd numbers less than 100
+//for (let i = 1 ; i <= 100; i+=2){
+//   console.log(i);
+//}
+
+//Coding challenge #3: Print the multiplication table with 7
+
+//for (let i = 1; i <=10 ; i++) {
+//    let resultado = i * 7;
+//    console.log( i + "X" + "7" + "=" + resultado);
+//}
+
+
