2c1711edc4fd4eca69e827d10f799f2214e93d5f
Matias Aramayo <matiasaramayo@hotmail.com>
2021-06-27 16:21:27 -0300

    modificaciones

--- clase_4_desafio/javascript.js
+++ clase_4_desafio/javascript.js
@@ -1,7 +1,20 @@
-let precio = parseFloat(prompt("Ingrese un monto"));
-let cuotas = parseFloat(prompt("Ingrese la cantidad de cuotas"));
 
-const calculo = (precio,cuotas) =>  precio/cuotas 
-const resultado = calculo(precio,cuotas)
 
-document.write("Usted debe pagar" + " "+ precio + " " + "en" + " " + cuotas + " " + "cuotas" + " " + "." + "Debera pagar por mes"  + resultado + " " + "pesos");
+
+
+
+
+
+
+//POsible arrow function
+//const calculoCero = (producto,cantidad) => producto * cantidad
+//const resultadoCero = calculoCero(producto,cantidad)
+//console.log(resultadoCero);
+
+//let precio = parseFloat(prompt("Ingrese un monto"));
+//let cuotas = parseFloat(prompt("Ingrese la cantidad de cuotas"));
+
+//const calculo = (precio,cuotas) =>  precio/cuotas 
+//const resultado = calculo(precio,cuotas)
+
+//document.write("Usted debe pagar" + " "+ precio + " " + "en" + " " + cuotas + " " + "cuotas" + " " + "." + "Debera pagar por mes"  + resultado + " " + "pesos");
---
+++ tokioSpirits/javascript.js
@@ -0,0 +1,31 @@
+let norteamericanas = 500;
+let anime = 450;
+let mechas = 350;
+let clasicos = 600;
+let deportes = 200;
+let romanticas = 900;
+
+alert(`Bienvenido a Tokio Spirits, te dejamos el catalogo para que puedas elegir que tipo de serie estas buscando
+menu: * Norteamericanas (Serie completa $500)
+      * Anime           (Serie completa $450)
+      * Mechas          (Serie completa $350)
+      * Clasicos        (Serie completa $600)
+      * Deportes        (Serie completa $200)
+      * Romanticas      (Serie completa $900)`);
+
+let producto = prompt(`Elegí un producto 
+menu: * norteamericanas 
+      * anime           
+      * mechas          
+      * clasicos        
+      * deportes        
+      * romanticas      
+      `);
+
+let cantidad = parseInt("Elige la cantidad de productos que desea llevar");
+
+switch (producto){
+    case "norteamericanas":
+        
+   
+}
