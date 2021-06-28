20db9c3c504f97bffd010a738f32e2db7e4bd9d3
Matias Aramayo <matiasaramayo@hotmail.com>
2021-06-28 18:10:10 -0300

    desafio complementario

---
+++ clase_4_desafio_complementario/javascript.js
@@ -0,0 +1,18 @@
+let precio = parseFloat(prompt("Ingrese un monto"));
+let cuotas = parseFloat(prompt("Ingrese la cantidad de cuotas"));
+let iva = 0.21;
+
+//Devuelve el precio con el iva incluido. Podría llamarse precioConIva
+const precioFinal = (precio,iva) => (precio + (precio*iva));
+
+// calculoUno podría tener un nombre más expresivo como cuotasSinIva
+const calculoUno = (precio,cuotas) => precio/cuotas;
+
+// Lo mismo impuestos podría llamar cuotasConIva
+const impuestos = (resultado,iva) => (resultado +(resultado * iva));
+
+// Resultado final podría resolverse llamando a la función cuotasConIva,
+// que a su vez llame a como primer parámetro a la función cuotasSinIva, y como
+// segundo parámetro a la variable iva
+const resultadoFinal = impuestos(resultado,iva);
+console.log("Resultado de cuotas con IVA" + " " + resultadoFinal);
