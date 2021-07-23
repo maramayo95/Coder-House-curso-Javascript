f31bdb0763dacb5b4981819a34b17661a4539679
Matias Aramayo <matiasaramayo@hotmail.com>
2021-07-23 18:16:24 -0300

    haciendo funcionar las alerts

---
+++ clase_8_desafio/javascript.js
@@ -0,0 +1,18 @@
+
+
+// Uniendo elementos de HTML con JS 
+// Haciendo Llamado a los inputs
+let nombre = document.querySelector("#nombre");
+let sexo = document.querySelector("#sexo");
+let edad = document.querySelector("#edad");
+let email = document.querySelector("#email");
+let historia = document.querySelector("#historia");
+
+// Haciendo llamado a las Alerts 
+let refNombre = document.querySelector("#alertNombre");
+let refSexo =  document.querySelector("alertSexo");
+let refEdad = document.querySelector("alertEdad");
+let refEmail = document.querySelector("alertEmail");
+let refHistoria = document.querySelector("alertHistoria");
+
+
