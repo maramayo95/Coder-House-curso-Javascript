3c25dacf47154ee313804fa61541a4a3f38e03f9
Matias Aramayo <matiasaramayo@hotmail.com>
2021-07-31 18:46:00 -0300

    definiendo variables en javascript

---
+++ clase_entrega_final/javascript.js
@@ -0,0 +1,24 @@
+
+class anime {
+    constructor (nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio; 
+    }
+}
+
+
+//Enlace con html 
+
+let airayuuki = document.querySelector('#1');
+let akira = document.querySelector('#2');
+let codeGeas = document.querySelector('#3');
+let dateAlive = document.querySelector('#4');
+let ghost = document.querySelector('#5');
+let evangelion = document.querySelector('#6');
+let psychoPass = document.querySelector('#7');
+let susei = document.querySelector('#8');
+let eightySix = document.querySelector('#9');
+
+
+// Eventos 
+
