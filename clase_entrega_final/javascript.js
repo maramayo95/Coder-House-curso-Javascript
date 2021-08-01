f041ef54b0236f5e2d3a84475eb3c2f8fb71066f
Matias Aramayo <matiasaramayo@hotmail.com>
2021-08-01 18:43:55 -0300

    construccion de objetos y correccion en los identificadores

--- clase_entrega_final/javascript.js
+++ clase_entrega_final/javascript.js
@@ -1,24 +1,42 @@
 
-class anime {
+// Creamos la clase Anime para que tome como datos el título  y el precio
+class Anime {
     constructor (nombre, precio){
         this.nombre = nombre;
-        this.precio = precio; 
+        this.precio = precio;
     }
 }
 
+// Creamos los objetos de mechas correspondientes 
+let animeAirayuuki = new Anime ('Airayuuki', 300)
+let animeAkira = new Anime ('Akira', 300);
+let animeCodeGeas = new Anime ('Code Geas' , 300);
+let animeDateAlive = new Anime ('Date Alive', 300);
+let animeGhost = new Anime ('Ghost', 300);
+let animeEvangelion = new Anime ('Evangelion' , 300);
+let animePsychoPass = new Anime ('Psycho Pass',300);
+let animeSusei = new Anime ('Susei',300);
+let anime86 = new Anime ('Eighty Six',300);
+
+
+//  Creamos el array Carrito  
+let carrito = []; 
 
 //Enlace con html 
 
-let airayuuki = document.querySelector('#1');
-let akira = document.querySelector('#2');
-let codeGeas = document.querySelector('#3');
-let dateAlive = document.querySelector('#4');
-let ghost = document.querySelector('#5');
-let evangelion = document.querySelector('#6');
-let psychoPass = document.querySelector('#7');
-let susei = document.querySelector('#8');
-let eightySix = document.querySelector('#9');
+let airayuuki = document.querySelector('#button1');
+let akira = document.querySelector('#button2');
+let codeGeas = document.querySelector('#button3');
+let dateAlive = document.querySelector('#button4');
+let ghost = document.querySelector('#button5');
+let evangelion = document.querySelector('#button6');
+let psychoPass = document.querySelector('#button7');
+let susei = document.querySelector('#button8');
+let eightySix = document.querySelector('#button9');
 
 
 // Eventos 
+
+
+
 
